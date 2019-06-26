08542f6e6e84634821eb619a101f873926becce8
Amit Singh <37627767+amitsin403@users.noreply.github.com>
2019-06-26 16:47:59 +0530

    Leap Year 1
    Print a leap year program in c programming

---
+++ Leap year.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int year;
+	printf("Enter a number");
+	scanf("%d",&year);
+	if (year%4)
+	printf("Not a leap year");
+	else if (year%100)
+	printf("Leap year");
+	else if (year%400)
+	printf("Not a leap year");
+	else 
+	printf("Leap year");
+	getch();
+}
